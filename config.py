f94c044299bec274da90e6a619459ea719de54c1
Glox <gloxieplayz@gmail.com>
2026-04-19 16:36:17 +0530

    Delete config.py

--- config.py
+++
@@ -1,2 +0,0 @@
-DATABASE_URL: str = "postgresql://postgres.yucpkpytprmpzahczsag:gh9L2maeAnKgMSJd@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres"
-DISCORD_TOKEN: str = "MTQ5NDc0MjAxMjUxNDE0MDMyMA.G_-Prn.mX6zNo_0DIGJoEBHMw2eW6vuLN1F-temITXWJs"
